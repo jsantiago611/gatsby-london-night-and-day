057e6eba3df5e6e5512f3f37fc667e2d52dd1817
jsantiago611 <36797222+jsantiago611@users.noreply.github.com>
2020-05-26 21:20:27 -0400

    Update siteConfig.js

--- siteConfig.js
+++ siteConfig.js
@@ -1,7 +1,7 @@
 module.exports = {
   name: 'Prayer in Life',
   shortName: '',
-  description: 'We're creating prayer guides for you as you pray for what matters the most',
+  description: 'We are creating prayer guides for you as you pray for what matters the most',
   url: 'https://www.jooplaan.com', // Domain of your site without prefix!
   prefix: '/',
   author: 'jooplaan',
