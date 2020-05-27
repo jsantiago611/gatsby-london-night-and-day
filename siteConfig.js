92a07da36b5ca6feb65914b4223aca97952370c4
jsantiago611 <36797222+jsantiago611@users.noreply.github.com>
2020-05-26 21:12:00 -0400

    Update siteConfig.js

--- siteConfig.js
+++ siteConfig.js
@@ -1,7 +1,7 @@
 module.exports = {
   name: 'Prayer in Life',
   shortName: '',
-  description: 'A bold, minimal theme for Gatsby, focused on clean typography & beautiful imagery',
+  description: 'We're creating prayer guides for you as you pray for what matters the most',
   url: 'https://www.jooplaan.com', // Domain of your site without prefix!
   prefix: '/',
   author: 'jooplaan',
